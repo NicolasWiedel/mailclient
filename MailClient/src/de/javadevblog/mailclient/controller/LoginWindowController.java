8358c6307acece93618868a27b84280660002194
Nicolas Wiedel <nwiedel@alte-hof-apotheke.com>
2020-11-26 08:24:32 +0100

    PW2

--- MailClient/src/de/javadevblog/mailclient/controller/LoginWindowController.java
+++ MailClient/src/de/javadevblog/mailclient/controller/LoginWindowController.java
@@ -77,7 +77,5 @@ public class LoginWindowController extends BaseController implements Initializab
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-//		tfMailAdress.setText("nicolaswiedel@java-devblog.de");
-		tfMailAdress.setText("niciwiedel@gmail.com");
 	}
 }
